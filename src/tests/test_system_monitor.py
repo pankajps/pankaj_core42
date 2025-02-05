b08f8b643d794402e7f97120ba7909d65649667c
Pankaj Singh <pankaj.singh@tesnus.ae>
2025-02-05 22:48:14 +0400

    Removed system_monitor

--- src/tests/test_system_monitor.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from pankaj_core42.utils.system_monitor import system_monitor
-
-def test_system_monitor(capsys):
-    """Test that system monitor prints CPU and memory usage."""
-    system_monitor()
-    captured = capsys.readouterr()
-    assert "CPU Usage" in captured.out
-
